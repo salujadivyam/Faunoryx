a6a4fc4556e3cf2ac1165a1892c661fa47efa3e4
Divyam Saluja <140640306+salujadivyam@users.noreply.github.com>
2026-09-01 12:49:58 +0530

    Added 2 output variables
    Added to build the SQL connection string in the python script which will allow us to get the server address via terraform output
    
    basically this is to avoid hardcoding

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,9 @@ output "eventhub_listen_connection_string" {
 output "eventhub_name" {
   value = azurerm_eventhub.hub.name
 }
+output "sql_server_fqdn" {
+  value = azurerm_mssql_server.sql_server.fully_qualified_domain_name
+}
+output "sql_database_name" {
+  value = azurerm_mssql_database.sql_db.name
+}
