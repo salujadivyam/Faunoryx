79bfb411c24f048791ff7bfba3a8883c50ce3ffd
Divyam Saluja <140640306+salujadivyam@users.noreply.github.com>
2026-09-06 22:56:57 +0530

    Bugs fixed

--- sql_writer.py
+++ sql_writer.py
@@ -2,19 +2,19 @@ import pyodbc
 
 class SQL:
     def __init__(self,server,username,database,password):
+        driver="{ODBC Driver 18 for SQL Server}"
         conn_str=f"Driver={driver};Server={server};Database={database};UID={username};PWD={password}"
         self.conn=pyodbc.connect(conn_str)
         self.cursor=self.conn.cursor()
-        driver="{ODBC Driver 18 for SQL Server}"
 
     def insert_telemetry(self,ping):
         self.cursor.execute( """INSERT INTO AnimalTelemetry(animal_id, timestamp, lat,lon, speed_kmph,still, speed_anomaly,outside_boundary)
-        VALUES(?,?,?,?,?,?,?,?)""",ping["animal_id"],ping["timestamp"],ping["lat"],ping["lon"],ping["speedkmph"],ping["still"],ping["speed_anomaly"],ping["putside_boundary"])
+        VALUES(?,?,?,?,?,?,?,?)""",ping["animal_id"],ping["timestamp"],ping["lat"],ping["lon"],ping["speedkmph"],ping["still"],ping["speed_anomaly"],ping["outside_boundary"])
         self.conn.commit()
 
     def insert_anomaly(self,ping,anomaly_type):
         self.cursor.execute("""INSERT INTO MovementAnomalies(animal_id,timestamp,lat,lon,anomaly_type)
-        VALUES(?,?,?,?,?))""",ping["animal_id"],ping["timestamp"],ping["lat"],ping["lon"],anomaly_type)        
+        VALUES(?,?,?,?,?)""",ping["animal_id"],ping["timestamp"],ping["lat"],ping["lon"],anomaly_type)        
         self.conn.commit()
 
     def close(self):
