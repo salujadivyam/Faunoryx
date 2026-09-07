915826056969efa4e9d31049283dcf226c6fc164
Divyam Saluja <140640306+salujadivyam@users.noreply.github.com>
2026-09-07 10:20:18 +0530

    Added azure functions
    Inclusion of Storage account, Service plan, and a function app

--- main.tf
+++ main.tf
@@ -105,3 +105,29 @@ resource "azurerm_mssql_firewall_rule" "allow_myip"{
   start_ip_address=var.my_ip
   end_ip_address=var.my_ip
 }
+
+resource "azurerm_storage_account" "faunostore" {
+  name="faunofuncstorage"
+  resource_group_name=azurerm_resource_group.rg.name
+  location=azurerm_resource_group.rg.location
+  account_tier="Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "faunoservice" {
+  name="faunofuncservice"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "faunofunc" {
+  name="alert-function-app"
+  location=azurerm_resource_group.rg.location
+  resource_group_name=azurerm_resource_group.rg.name
+  service_plan_id=azurerm_service_plan.faunoservice.id
+  storage_account_name=azurerm_storage_account.faunostore.name
+  storage_account_access_key=azurerm_storage_account.faunostore.primary_access_key
+  site_config {}
+}
